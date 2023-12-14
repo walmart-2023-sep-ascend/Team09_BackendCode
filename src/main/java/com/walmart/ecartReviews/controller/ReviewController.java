4735a38d782c2043c11f36e8cb85f7da082d3a00
SwethaGRaman <swethag196@gmail.com>
2023-12-14 11:36:08 +0530

    Update ReviewController.java

--- src/main/java/com/walmart/ecartReviews/controller/ReviewController.java
+++ src/main/java/com/walmart/ecartReviews/controller/ReviewController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 @RestController
+@CrossOrigin(origins = "http://172.172.241.64:3000")
 @RequestMapping("/api/product")
 public class ReviewController {
 
