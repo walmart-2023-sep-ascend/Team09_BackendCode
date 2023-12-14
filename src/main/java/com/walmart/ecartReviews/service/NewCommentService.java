f693862a2362b611f755bbab613efc69933c33b2
SwethaGRaman <swethag196@gmail.com>
2023-12-14 07:20:28 +0530

    Update NewCommentService.java

--- src/main/java/com/walmart/ecartReviews/service/NewCommentService.java
+++ src/main/java/com/walmart/ecartReviews/service/NewCommentService.java
@@ -7,10 +7,12 @@ import com.walmart.ecartReviews.repository.NewCommentRepository;
 import com.walmart.ecartReviews.repository.ProductRepository;
 
 import com.walmart.ecartReviews.repository.ReviewRepository;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
