55ff34461436ea88949849fb8bd87b79cb118d67
SwethaGRaman <swethag196@gmail.com>
2023-12-14 11:38:39 +0530

    Update ProductController.java

--- src/main/java/com/walmart/ecartReviews/controller/ProductController.java
+++ src/main/java/com/walmart/ecartReviews/controller/ProductController.java
@@ -4,10 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.walmart.ecartReviews.model.Product;
 import com.walmart.ecartReviews.service.ProductService;
